583c2a4e48a38c7c01e45870865ab13df06ff800
Jorge Alcover <jorgealcover@paucasesnovescifp.cat>
2026-05-10 18:00:28 +0200

    feat: acabadas las categorias

--- script/categories.js
+++ script/categories.js
@@ -15,4 +15,33 @@ document.getElementById("Categories").addEventListener("submit", function(event)
     let categories = JSON.parse(localStorage.getItem("categories")) || [];
     categories.push(categoria);
     localStorage.setItem("categories", JSON.stringify(categories));
+});
+
+
+function carregarCategories() {
+    const categories = JSON.parse(localStorage.getItem("categories")) || [];
+    const categoriesList = document.getElementById("MostrarCategories");
+    categoriesList.innerHTML = "";
+    categories.forEach(function(categoria) {
+        const div = document.createElement("div");
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        div.style.backgroundColor = categoria.color;
+        div.style.borderRadius = "10px";
+        div.style.width = "10px";
+        div.style.height = "10px";
+        
+        deleteButton.textContent = "Eliminar";
+        deleteButton.classList.add("deleteButton");
+        deleteButton.addEventListener("click", function() {
+            categories.splice(categories.indexOf(categoria), 1);
+            localStorage.setItem("categories", JSON.stringify(categories));
+            carregarCategories();
+        });
+        li.appendChild(div);
+        li.appendChild(document.createTextNode(categoria.nom));
+        li.appendChild(deleteButton);
+        categoriesList.appendChild(li);
     });
+}
+carregarCategories();
